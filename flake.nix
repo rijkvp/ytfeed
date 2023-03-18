09e4e6df631f80d57c6bbbaba4f47b16ad7f3bec
rijkvp <rijk@rijkvp.nl>
2023-03-18 12:37:21 +0100

    Update dependencies & add Nix flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        inherit (nixpkgs) lib;
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      with pkgs;
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "ytfeed";
+          inherit ((lib.importTOML (self + "/Cargo.toml")).package) version;
+          src = self;
+          cargoLock.lockFile = self + "/Cargo.lock";
+        };
+        devShells.default = mkShell {
+          nativeBuildInputs = with pkgs; [ rustc cargo cargo-dist cargo-outdated ];
+        };
+      }
+    );
+}
+
